795b164c315c23c83b5f2feffe1f6ba474296571
Mohamed Badawy <33234669+gommido@users.noreply.github.com>
2025-05-21 17:37:26 +0300

    Update binary_search.dart

--- lib/tylo/core/helper_functions/binary_search.dart
+++ lib/tylo/core/helper_functions/binary_search.dart
@@ -1,18 +1,1 @@
 
-import '../../domain/entities/app_user.dart';
-
-String binarySearchExists(List<AppUser> sortedList, String target) {
-  int min = 0;
-  int max = sortedList.length - 1;
-  while (min <= max) {
-    int mid = min + ((max - min) >> 1);
-    if (sortedList[mid].phone == target) {
-      return sortedList[mid].id; // Target found
-    } else if (sortedList[mid].phone!.compareTo(target) < 0) {
-      min = mid + 1;
-    } else {
-      max = mid - 1;
-    }
-  }
-  return ''; // Target not found
-}
